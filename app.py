ba8080ffa9a075542ed8ae8a4f72cde3c2cbc1a5
DMGkim <conan8548@gmail.com>
2023-07-11 20:13:13 +0900

    commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+import cv2
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+app = Flask(__name__)
+
+def analyze_leaf(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    average_color = image.mean(axis=0).mean(axis=0)
+    green_percentage = (average_color[1] / 255) * 100
+    return green_percentage
+
+def predict_growing_days(temperature, humidity, soil_moisture):
+    training_data = {
+        '온도': [20, 25, 30, 35, 40],
+        '습도': [30, 35, 40, 45, 50],
+        '토양수분': [0.2, 0.4, 0.6, 0.8, 1.0],
+        '재배_가능일': [60, 65, 70, 75, 80]
+    }
+    df = pd.DataFrame(training_data)
+    X = df[['온도', '습도', '토양수분']]
+    y = df['재배_가능일']
+    model = LinearRegression()
+    model.fit(X, y)
+    new_data = {
+        '온도': [temperature],
+        '습도': [humidity],
+        '토양수분': [soil_moisture]
+    }
+    df_new = pd.DataFrame(new_data)
+    predicted_days = model.predict(df_new)
+    return predicted_days[0]
+
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    if 'image' not in request.files:
+        return 'No image file received', 400
+
+    image_file = request.files['image']
+    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
+    leaf_status = analyze_leaf(image)
+
+    temperature = float(request.form.get('temperature'))
+    humidity = float(request.form.get('humidity'))
+    soil_moisture = float(request.form.get('soil_moisture'))
+    predicted_days = predict_growing_days(temperature, humidity, soil_moisture)
+
+    response = {
+        'leaf_status': leaf_status,
+        'predicted_days': predicted_days
+    }
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1234)
