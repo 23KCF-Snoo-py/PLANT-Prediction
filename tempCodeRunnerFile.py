a0a8003630fe47021980386f3e1cb1a0ca270773
DMGkim <conan8548@gmail.com>
2023-07-12 23:45:59 +0900

    last

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import cv2
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-import numpy as np
 
 app = Flask(__name__)
 
@@ -14,20 +13,26 @@ def analyze_leaf(image):
 
 def predict_growing_days(temperature, humidity, soil_moisture):
     training_data = {
-        '온도': [20, 25, 30, 35, 40],
-        '습도': [30, 35, 40, 45, 50],
-        '토양수분': [0.2, 0.4, 0.6, 0.8, 1.0],
-        '재배_가능일': [60, 65, 70, 75, 80]
+        'temp': [20, 25, 30, 35, 40],
+        'humi': [30, 35, 40, 45, 50],
+        'soilMoisture': [0.2, 0.4, 0.6, 0.8, 1.0],
+        'Lettuce': [60, 65, 70, 75, 80],
+        'Basil': [50, 55, 60, 65, 70],
+        'Strawberry': [70, 75, 80, 85, 90],
+        'Tomato': [80, 85, 90, 95, 100],
+        'Herb': [55, 60, 65, 70, 75],
+        'Celery': [65, 70, 75, 80, 85],
+        'Kale': [45, 50, 55, 60, 65]
     }
     df = pd.DataFrame(training_data)
-    X = df[['온도', '습도', '토양수분']]
-    y = df['재배_가능일']
+    X = df[['temp', 'humi', 'soilMoisture']]
+    y = df[['Lettuce', 'Basil', 'Strawberry', 'Tomato', 'Herb', 'Celery', 'Kale']]
     model = LinearRegression()
     model.fit(X, y)
     new_data = {
-        '온도': [temperature],
-        '습도': [humidity],
-        '토양수분': [soil_moisture]
+        'temp': [temperature],
+        'humi': [humidity],
+        'soilMoisture': [soil_moisture]
     }
     df_new = pd.DataFrame(new_data)
     predicted_days = model.predict(df_new)
@@ -35,16 +40,24 @@ def predict_growing_days(temperature, humidity, soil_moisture):
 
 @app.route('/upload_sensor_data', methods=['POST'])
 def process_data():
-    temperature = float(request.form.get('temp'))
+    temperature = float(request.form.get('Temp'))
     humidity = float(request.form.get('humi'))
-    soil_moisture = float(request.form.get('soilMoisture'))
+    soil_moisture = float(request.form.get('soil'))
     predicted_days = predict_growing_days(temperature, humidity, soil_moisture)
-
+    app.logger.debug("Received data - Temperature: {}, Humidity: {}, Soil Moisture: {}".format(temperature, humidity, soil_moisture))
     response = {
-        'predicted_days': predicted_days
+        'predicted_days': {
+            'Lettuce': predicted_days[0],
+            'Basil': predicted_days[1],
+            'Strawberry': predicted_days[2],
+            'Tomato': predicted_days[3],
+            'Herb': predicted_days[4],
+            'Celery': predicted_days[5],
+            'Kale': predicted_days[6]
+        }
     }
-    return response
-    
+    return jsonify(response)
+
 @app.route('/upload_image', methods=['POST'])
 def upload_image():
     image_file = request.files['image']
@@ -52,12 +65,12 @@ def upload_image():
         image_path = 'path/to/save/image.jpg'
         image_file.save(image_path)
     leaf_status = analyze_leaf(image_file)
-    
+
     response = {
         'leaf_status': leaf_status,
     }
-    return response
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1234)
+    app.run(host='0.0.0.0', port=1234, debug=True)
