d7f67cbce9a029530d455e80ec883573dee8432a
DMGkim <conan8548@gmail.com>
2023-07-12 21:51:03 +0900

    test

--- app.py
+++ app.py
@@ -33,31 +33,31 @@ def predict_growing_days(temperature, humidity, soil_moisture):
     predicted_days = model.predict(df_new)
     return predicted_days[0]
 
-# @app.route('/upload_sensor_data', methods=['POST'])
-# def process_data():
-#     temperature = float(request.form.get('temp'))
-#     humidity = float(request.form.get('humi'))
-#     soil_moisture = float(request.form.get('soilMoisture'))
-#     predicted_days = predict_growing_days(temperature, humidity, soil_moisture)
-#     app.logger.debug("Received data - Temperature: {}, Humidity: {}, Soil Moisture: {}".format(temperature, humidity, soil_moisture))
-#     response = {
-#         'predicted_days': predicted_days
-#     }
-#     return response
-    
-@app.route('/upload_sensor_data', methods=['POST','GET'])
-def upload_sensor_data():
+@app.route('/upload_sensor_data', methods=['POST'])
+def process_data():
     temperature = float(request.form.get('temp'))
     humidity = float(request.form.get('humi'))
     soil_moisture = float(request.form.get('soilMoisture'))
-
-    # 데이터 처리 및 원하는 동작 수행
-    # 예시로 간단히 로그에 출력하고 응답을 생성하는 코드를 추가했습니다.
+    predicted_days = predict_growing_days(temperature, humidity, soil_moisture)
     app.logger.debug("Received data - Temperature: {}, Humidity: {}, Soil Moisture: {}".format(temperature, humidity, soil_moisture))
     response = {
-        'message': 'Data received and processed successfully'
+        'predicted_days': predicted_days
     }
     return response
+    
+# @app.route('/upload_sensor_data', methods=['POST','GET']  )
+# def upload_sensor_data():
+#     temperature = float(request.form.get('temp'))
+#     humidity = float(request.form.get('humi'))
+#     soil_moisture = float(request.form.get('soilMoisture'))
+
+#     # 데이터 처리 및 원하는 동작 수행
+#     # 예시로 간단히 로그에 출력하고 응답을 생성하는 코드를 추가했습니다.
+#     app.logger.debug("Received data - Temperature: {}, Humidity: {}, Soil Moisture: {}".format(temperature, humidity, soil_moisture))
+#     response = {
+#         'message': 'Data received and processed successfully'
+#     }
+#     return response
 
 
 
@@ -77,3 +77,4 @@ def upload_image():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1234, debug=True)
+    
